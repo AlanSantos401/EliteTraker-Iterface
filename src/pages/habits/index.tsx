d89f0161821e7a81aeb19108be7351aa58d15284
Alan <alandsantos54@gmail.com>
2025-08-21 13:39:58 -0300

    DevClub Elite - Elite Traker pt.4 - Aula 14

--- src/pages/habits/index.tsx
+++ src/pages/habits/index.tsx
@@ -1,5 +1,26 @@
+import { Send, Trash2 } from "lucide-react";
+import styles from "./styles.module.css";
+
 export function Habits() {
-    return (
-        <h1>Hábitos Diários</h1>
-    )
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>
+				<header>
+					<h1>Hábitos Diários</h1>
+					<span>Hoje, 21 de Agosto</span>
+				</header>
+				<div className={styles.input}>
+					<input placeholder="Digite aqui um novo hábito" type="text" />
+                    <Send />
+				</div>
+				<div className={styles.habit}>
+					<p>estudar espanhol</p>
+					<div>
+						<input type="checkbox" />
+						<Trash2 />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
